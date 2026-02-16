999d83b809aa2cdd4eac9e1cbd4df6b0a036bf86
mohamed <mohamedwael3346@gmail.com>
2026-02-16 19:25:26 +0200

    Initial backend setup with Express server

---
+++ api/src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    message: "API is running",
+  });
+});
+
+module.exports = app;
